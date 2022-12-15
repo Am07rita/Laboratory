c5094791cd6a4e427b1f430ca5c40385a2005b28
Am07rita <120620473+Am07rita@users.noreply.github.com>
2022-12-15 13:49:57 +0530

    Create Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,23 @@
+//WAP to check whether a charcter is an alphabet or not.
+
+package basic;
+
+import java.util.Scanner;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a no.");
+		int i,num=sc.nextInt();;
+		for(i=1;i<=10;i++) {
+		
+			System.out.println(num+"*"+i+"="+(num*i));
+		}
+		
+		}
+     
+	}
+
+
