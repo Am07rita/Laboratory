40cc9ac3a55b47c8b06a454f79664b5a2151b132
Am07rita <120620473+Am07rita@users.noreply.github.com>
2022-12-15 12:22:06 +0530

    Create First.java

---
+++ Assignment 15-12-22/First.java
@@ -0,0 +1,19 @@
+//Wap to print hello 5 times
+
+package basic;
+public class First {
+
+	public static void main(String[] args) {
+	int i;//counter value	
+    for(i=0; i<=4; i++) //increment
+    {
+       System.out.println("Hello");	
+    }
+    System.out.println();
+    for(i=5; i>0; i--) //decrement
+    {
+       System.out.println("world");	
+    }	
+	}
+
+}
