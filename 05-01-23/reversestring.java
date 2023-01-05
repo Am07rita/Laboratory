436a9b8d78d2cef605d235d40de13758f2d91001
Amrita kumari <120620473+Am07rita@users.noreply.github.com>
2023-01-05 11:18:03 +0530

    Create reversestring.java

---
+++ 05-01-23/reversestring.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class Reversestring {
+
+	public static void main(String[] args) {
+		StringBuilder builder=new StringBuilder("hello");
+		builder.reverse();
+		System.out.println(builder);
+	}
+
+}
