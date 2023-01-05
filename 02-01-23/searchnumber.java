7935183b4700a1ec5c2bf73074aff8e7c4c3edc1
Amrita kumari <120620473+Am07rita@users.noreply.github.com>
2023-01-05 11:12:39 +0530

    Create searchnumber.java

---
+++ 02-01-23/searchnumber.java
@@ -0,0 +1,47 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Searchnumber {
+
+	public static void main(String[] args) {
+	int size,i;
+    Scanner sc=new Scanner (System.in);
+    int pos=-1;
+   // boolean flag=false;//using boolean variable flag and initializing it as false
+    
+    System.out.println("Enter the size of array");
+    size=sc.nextInt();
+    int ar[]=new int[size];
+    System.out.println("Enter "+size+" elements");
+    for(i=0;i<size;i++)
+    {
+    	ar[i]=sc.nextInt();
+    }
+    System.out.println("Entere the elements you want to search");
+	int search=sc.nextInt();
+	
+	
+	//searching the element
+	for(i=0;i<size;i++)
+	{
+		if( ar[i]==search)
+		{
+			//flag= true;//if element is found setting flag to be true
+			pos=i;
+			break;
+		}
+	}
+	if (pos!=-1)
+	//if (flag==true)
+	{
+		//System.out.println("The element "+search+" is present in: "+(i+1)+" position");
+		System.out.println("The element "+search+" is present in:  "+(pos+1)+" position");
+	}
+	else
+	{
+		System.out.println("Data not found!!");
+	}
+	}
+
+}
