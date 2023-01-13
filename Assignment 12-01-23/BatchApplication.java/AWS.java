0de929aa76dca4fe9adb7483e17ba665f98e5c12
Amrita kumari <120620473+Am07rita@users.noreply.github.com>
2023-01-13 11:27:08 +0530

    Create AWS.java

---
+++ Assignment 12-01-23/BatchApplication.java/AWS.java
@@ -0,0 +1,52 @@
+package Batchapplication;
+
+public class AWS {
+	private int stdId;
+	private String stdName;
+	private long phone;
+	private String email;
+
+	public int getStdId() {
+		return stdId;
+	}
+	
+	public void setStdId(int stdId) {
+		this.stdId = stdId;
+	}
+	
+	public String getStdName() {
+		return stdName;
+	}
+	
+	public void setStdName(String stdName) {
+		this.stdName = stdName;
+	}
+	
+	public long getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public AWS() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public AWS(int stdId, String stdName, long phone, String email) {
+		super();
+		this.stdId = stdId;
+		this.stdName = stdName;
+		this.phone = phone;
+		this.email = email;
+	}
+	
+}
